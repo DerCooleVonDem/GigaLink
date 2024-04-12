448c35b44aea6812774d3c7a2023e41b1b0fdd98
Jonas <jonaswindmann@gmail.com>
2024-04-12 08:25:06 +0200

    Clean

--- src/Main.php
+++ src/Main.php
@@ -4,13 +4,10 @@ declare(strict_types=1);
 
 namespace DerCooleVonDem\GigaLink;
 
+use JonasWindmann\GigaLink\log\CustomMainLogger;
 use pocketmine\plugin\PluginBase;
 
-class Main extends PluginBase{
-
-    public function onEnable(): void
-    {
-
-    }
+class Main extends PluginBase
+{
 
 }
